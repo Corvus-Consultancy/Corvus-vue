fd306ae8f85499f8720acd6c50d78fad3b7b2681
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-12 21:00:57 +0100

    🍱 Added new icons

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faPhone)
-
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faAngleLeft)
+library.add(faAngleRight)
 
 createApp(App)
   .use(store)
