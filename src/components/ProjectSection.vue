df9a5605b74a1f16fc09f5f90859fe33a11ff6d5
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-12 21:01:45 +0100

    ✨ Added project section

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -1,32 +1,68 @@
 <template>
-  <section class="project">
+  <section id="projects">
+    <h2>Projects</h2>
 
+    <div class="projects">
+      <a href="#">
+        <font-awesome-icon icon="angle-left" />
+      </a>
+
+      <section class="project">
+        <img src="@/assets/zonneveranda.png" />
+
+        <div>
+          <h3>Zonneveranda</h3>
+          <p>ZonneVeranda reached out to us to create promotion material for an unexpected opportunity to present themselves at a fair. This fair however, was taking place within two days of us hearing about the project. Considering the prompt nature of the assignment, we allocated our time and managed to create an array of captivating designs within the same day.</p>
+        </div>
+      </section>
+
+      <a href="#">
+        <font-awesome-icon icon="angle-right" />
+      </a>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: 'ProjectSection',
-  props: {
-    msg: String
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+h2 {
+  font-size: 2.8rem;
+  font-weight: 400;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.projects {
+  width: 80%;
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin: 0 5rem;
+    font-size: 1.5rem;
+  }
+
+  .project {
+    display: flex;
+
+    img {
+      align-self: center;
+      margin-right: 3rem;
+    }
+
+    h3 {
+      margin: 0 0 .5rem;
+      font-size: 1.8rem;
+      font-weight: 600;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+    }
+  }
 }
 </style>
