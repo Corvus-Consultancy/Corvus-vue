0afc3bb529ef1c2b4b5800fbd839c0bf6a9c08c1
Sjors van Holst <sjorsvanholst@gmail.com>
2022-03-12 20:01:51 +0100

    💄 Added home content

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -1,6 +1,15 @@
 <template>
-  <section class="home">
+  <section id="home">
+    <h1>
+      <span>Corvus</span>
+      <span>Consultancy</span>
+    </h1>
 
+    <p>Creating innovative solutions for <strong>your</strong> business.</p>
+
+    <a href="#projects">
+      <span>Read more</span>
+    </a>
   </section>
 </template>
 
@@ -15,18 +24,113 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+h1 {
+  color: #fff;
+  margin: 0;
+  display: flex;
+
+  font-size: 8rem;
+  text-align: center;
+  flex-direction: column;
+
+  span:first-of-type {
+    font-weight: 500;
+  }
+  
+  span:last-of-type {
+    font-weight: 100;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+p {
+  color: #fff;
+
+  font-size: 1.5rem;
+  font-weight: 300;
+
+  strong {
+   color: #880000; 
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
-  color: #42b983;
+  width: 20%;
+  color: #fff;
+  height: 40px;
+  position: relative;
+  text-decoration: none;
+
+  &:after,
+  &:before {
+    right: 0;
+    bottom: 0;
+    content: "";
+    position: absolute;
+    background: white;
+    transition: all 0.3s ease;
+  }
+
+  &:before {
+    width: 2px;
+    height: 50%;
+  }
+
+  &:after {
+    width: 20%;
+    height: 2px;
+  }
+
+  &:hover {
+    &:before {
+      height: 100%;
+      border: #880000;
+      background: #880000;
+    }
+    
+    &:after {
+      width: 100%;
+      background: #880000;
+    }
+  }
+
+  span {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    position: relative;
+    
+    align-items: center;
+    justify-content: center;
+
+    &:after,
+    &:before {
+      top: 0;
+      left: 0;
+      content: "";
+      position: absolute;
+      background: white;
+      transition: all 0.3s ease;
+    }
+
+    &:after {
+      width: 20%;
+      height: 2px;
+    }
+
+    &:before {
+      width: 2px;
+      height: 50%;
+    }
+
+    &:hover {
+      &:after {
+        width: 100%;
+      }
+
+      &:before {
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
